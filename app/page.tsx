3a3c7ead2062610ee775fcef9eed96e8ebb77804
Andy Wilson <31218527+The-Code-Monkey@users.noreply.github.com>
2023-04-28 13:35:42 +0100

    hide other pages as not setup

--- app/page.tsx
+++ app/page.tsx
@@ -6,26 +6,26 @@ export default function Home() {
     <div className={styles.container}>
     <main className={styles.main}>
       <div className={styles.center}>
-        <Link href='/rmc'>
-        <h1 className={styles.title}>
-          RMC <br /> Leaderboard
-        </h1>
-        </Link>
+        {/*<Link href='/rmc'>*/}
+        {/*<h1 className={styles.title}>*/}
+        {/*  RMC <br /> Leaderboard*/}
+        {/*</h1>*/}
+        {/*</Link>*/}
         <Link href='/rms'>
-        <h1 className={styles.title}>
-          RMS <br /> Leaderboard
-        </h1>
-        </Link>
-        <Link href='/rmct'>
           <h1 className={styles.title}>
-            RMCT <br /> Leaderboard
+            RMS <br /> Leaderboard
           </h1>
         </Link>
-        <Link href='/submit'>
-          <h1 className={styles.title}>
-            Submit <br /> Record
-          </h1>
-        </Link>
+        {/*<Link href='/rmct'>*/}
+        {/*  <h1 className={styles.title}>*/}
+        {/*    RMCT <br /> Leaderboard*/}
+        {/*  </h1>*/}
+        {/*</Link>*/}
+        {/*<Link href='/submit'>*/}
+        {/*  <h1 className={styles.title}>*/}
+        {/*    Submit <br /> Record*/}
+        {/*  </h1>*/}
+        {/*</Link>*/}
       </div>
     </main>
     </div>
