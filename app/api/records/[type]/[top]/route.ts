ce45d9746c0d6b96e661f67b3188e2cd28f06687
Andy Wilson <andy@builtbypixel.com>
2023-09-11 16:49:55 +0100

    fix: some other stuff

--- app/api/records/[type]/[top]/route.ts
+++ app/api/records/[type]/[top]/route.ts
@@ -1,17 +1,21 @@
-import supabase from "@/app/supabase";
-import {NextResponse} from "next/server";
+import supabase from '@/app/supabase';
+import { NextResponse } from 'next/server';
 
-export async function GET(request: Request, { params }: { params: { top: string, type: string }}) {
-    const num = parseInt(params.top, 10);
+export async function GET(
+  request: Request,
+  { params }: { params: { top: string; type: string } }
+) {
+  const num = parseInt(params.top, 10);
 
-    const { data, error } = await supabase.from(params.type).select("*").order('ats', { ascending: false }).limit(num ?? 10);
+  const { data, error } = await supabase
+    .from(params.type)
+    .select('*')
+    .order('ats', { ascending: false })
+    .limit(num ?? 10);
 
-    if (error) {
-        return new Response(`Something Went Wrong`, { status: 400 });
-    }
+  if (error) {
+    return new Response(`Something Went Wrong`, { status: 400 });
+  }
 
-    return NextResponse.json(
-        { data, },
-    { status: 200 }
-    )
+  return NextResponse.json({ data }, { status: 200 });
 }
